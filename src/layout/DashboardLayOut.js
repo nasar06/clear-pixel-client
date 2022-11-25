8e1081ff208073ad68c4dbb28b8b29d1f569d0a5
nasar06 <nasar5847@gmail.com>
2022-11-25 20:12:52 +0600

    useSeller hook created

--- src/layout/DashboardLayOut.js
+++ src/layout/DashboardLayOut.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { AuthContext } from '../Context/AuthProvider/AuthProvider';
+import Loader from '../pages/shared/Loader/Loader';
 import TopNavbar from '../pages/shared/TopNavbar/TopNavbar';
+import useAdmin from '../Hooks/Admin/useAdmin';
+import useSeller from '../Hooks/seller/useSeller'
 
 const DashboardLayOut = () => {
+    const { user, isLoading } = useContext(AuthContext)
+    const [isAdmin, isAdminLoading] = useAdmin(user?.email)
+    const [isSeller, isSellerLoader] = useSeller(user?.email)
+
+
+    if (isAdminLoading || isLoading || isSellerLoader) {
+        return <Loader></Loader>
+    }
     return (
         <div>
             <TopNavbar></TopNavbar>
@@ -16,15 +28,25 @@ const DashboardLayOut = () => {
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 bg-base-100 text-base-content">
                         {/* <!-- Sidebar content here --> */}
-                        <Link className='mb-5 text-xl font-bold underline' to='/dashboard/myOrders'>My Orders</Link>
-                        <Link className='mb-5 text-xl font-bold underline' to='/dashboard/myProducts'>My Products</Link>
+                        <Link to='/dashboard/myOrders' className='mb-5 text-xl font-bold underline'>My Orders</Link>
+                        <Link to='/dashboard/myProducts' className='mb-5 text-xl font-bold underline' >My Products</Link>
                         {/* <Link className='mb-5 text-xl font-bold underline' to='/dashboard/myWishlist'>My WishList</Link> */}
-                        <Link className='mb-5 text-xl font-bold underline' to='/dashboard/addProduct'>Add Product</Link>
-                        <Link className='mb-5 text-xl font-bold underline' to='/dashboard/allSellers'>All Sellers</Link>
-                        <Link className='mb-5 text-xl font-bold underline' to='/dashboard/allBuyers'>All Buyers</Link>
-                        
-                        
-                        
+
+                        {
+                            isSeller &&
+                            <Link to='/dashboard/addProduct' className='mb-5 text-xl font-bold underline'>Add Product</Link>
+
+                        }
+                        {
+                            isAdmin &&
+                            <>
+                                <Link to='/dashboard/allSellers' className='mb-5 text-xl font-bold underline'>All Sellers</Link>
+                                <Link to='/dashboard/allBuyers' className='mb-5 text-xl font-bold underline'>All Buyers</Link>
+                            </>
+                        }
+
+
+
                     </ul>
 
                 </div>
